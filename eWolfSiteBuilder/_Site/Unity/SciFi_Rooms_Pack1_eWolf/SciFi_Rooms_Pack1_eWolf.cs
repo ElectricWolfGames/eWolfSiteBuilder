c81afacf8b683c7734d3f6a9426062f15b28597d
Karl Gillott <KarlGillott@googleMail.com>
2022-06-15 18:35:40 +0100

    update rooms details

--- eWolfSiteBuilder/_Site/Unity/SciFi_Rooms_Pack1_eWolf/SciFi_Rooms_Pack1_eWolf.cs
+++ eWolfSiteBuilder/_Site/Unity/SciFi_Rooms_Pack1_eWolf/SciFi_Rooms_Pack1_eWolf.cs
@@ -39,8 +39,10 @@ namespace eWolfSiteBuilder._Site.Unity.SciFi_Rooms_Pack1_eWolf
             items.Add(new HTMLIndexedItems("Overview", CreateOverview));
             items.Add(new HTMLIndexedItems("Download", CreateDownload));
             items.Add(new HTMLIndexedItems("Create First Rooms", CreateCreateFirstRooms));
+            items.Add(new HTMLIndexedItems("Player setup", CreatePlayerSetUp));
 
             //items.Add(new HTMLIndexedItems("Connection Types", CreateConnectionTypes));
+            //items.Add(new HTMLIndexedItems("Lighting", CreateLighting));
             //items.Add(new HTMLIndexedItems("Randomize All Fillers", CreateRandomizeAllFillers));
 
             items.Add(new HTMLIndexedItems("Support", PageDetailsHelper.CreateSupport));
@@ -179,17 +181,17 @@ namespace eWolfSiteBuilder._Site.Unity.SciFi_Rooms_Pack1_eWolf
             outer.NewLine();
             outer.NewLine();
 
-            outer.TextBoldText("", "1. Place all objects under right parents", " Will place all the rooms in to level folders");
+            outer.OtherDetails("1. Place all objects under right parents", " Will place all the rooms in to level folders");
             outer.NewLine();
             outer.Text("This is to just make things easyer to find if you have a map with different levels");
             outer.NewLine();
             outer.ImageCenter("RoomsLevel00Folder.PNG", 45);
             outer.NewLine();
             outer.NewLine();
-            outer.TextBoldText("", "2. Randomize All Fillers", " Will add extra random details to some rooms (see more later)");
+            outer.OtherDetails("2. Randomize All Fillers", " Will add extra random details to some rooms (see more later)");
             outer.NewLine();
             outer.NewLine();
-            outer.TextBoldText("", "3. Add Missing Doors & Effects", " Will add the doors to the map");
+            outer.OtherDetails("3. Add Missing Doors & Effects", " Will add the doors to the map");
             outer.NewLine();
             outer.NewLine();
             options = new HTMLBuilder();
@@ -200,6 +202,31 @@ namespace eWolfSiteBuilder._Site.Unity.SciFi_Rooms_Pack1_eWolf
             left.ImageRight("AddedDoor.PNG", 65);
             right.ImageLeft("AddedDoorList.PNG.", 75);
             outer.Text(options.Output());
+
+            outer.Text("It's created the door as a Fake door, this is due to the door leading now where.");
+            outer.NewLine();
+            outer.Text("And will not open.");
+            outer.NewLine();
+            outer.NewLine();
+            outer.OtherDetails("4. Generate Lighting", " Will bake in the lighting for the rooms.");
+            outer.NewLine();
+            outer.Bold("NOTE: Scene need to be set to use light map/backing - see Setting up Lighting section");
+            outer.NewLine();
+            outer.Text("All lights in prefabs are set-up as Baked.");
+            outer.NewLine();
+            outer.NewLine();
+            options = new HTMLBuilder();
+            left = new HTMLSection("col-md-6");
+            right = new HTMLSection("col-md-6");
+            options.SetTwoSections(left, right);
+
+            left.Text("Before Bake");
+            left.ImageRight("PreLightBake.PNG", 80);
+
+            right.Text("After Bake");
+            right.ImageLeft("PostLightBake.PNG.", 80);
+            outer.Text(options.Output());
+            outer.Text("While, not a lot of different - it's much faster as the lighting is fully backed in.");
 
             outer.EndTextCenter();
 
@@ -224,6 +251,50 @@ namespace eWolfSiteBuilder._Site.Unity.SciFi_Rooms_Pack1_eWolf
             // what this pack can do!
 
             outer.NewLine();
+            outer.EndTextCenter();
+
+            return outer.Output();
+        }
+
+        private string CreatePlayerSetUp(string arg)
+        {
+            HTMLBuilder outer = new HTMLBuilder();
+            outer.StartTextCenter();
+            outer.NewLine();
+
+            outer.NewLine();
+            outer.Text("To make the doors open you just need two things");
+            outer.NewLine();
+            outer.NewLine();
+            outer.TextBoldText("Your player object needs to have the script ", "RoomInteractor", " ");
+            outer.NewLine();
+            outer.TextBoldText("And the script ", "MainGameFlow", " anywhere in the project");
+            outer.NewLine();
+            outer.NewLine();
+            outer.Text("We have prefabs for both. ");
+            outer.NewLine();
+            outer.NewLine();
+            outer.OtherDetails("MainGameFlow_pf", "This can live anywhere in the scene");
+            outer.OtherDetails("TempPlayer_pf", "This needs to be under map_pf object.(This is just a camera, no player control)");
+            outer.NewLine();
+            outer.NewLine();
+            outer.ImageCenter("AddedPlayerMain.PNG", 35);
+            outer.NewLine();
+            outer.Text("Once both are added, You can run the game");
+            outer.NewLine();
+            outer.ImageCenter("RunningDoorClosed.PNG", 65);
+            outer.NewLine();
+            outer.NewLine();
+            outer.Text("When you move the player object towards the door it will open");
+            outer.NewLine();
+            outer.ImageCenter("RunningDoorOpen.PNG", 65);
+            outer.NewLine();
+            outer.Text("And the room behind the door will unhide, but for the door opens");
+            outer.NewLine();
+            outer.NewLine();
+            outer.Text("If you move more in to the room, the door will close and hide the rooms behind it.");
+            outer.ImageCenter("RunningDoorInRoom.PNG", 65);
+
             outer.EndTextCenter();
 
             return outer.Output();
