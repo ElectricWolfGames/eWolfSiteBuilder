d1987862fb1cea7bd6628ed864eef0830c85d3b7
Karl Gillott <KarlGillott@googleMail.com>
2022-07-07 07:03:22 +0100

    Updated Rooms

--- eWolfSiteBuilder/_Site/Unity/SciFiModularRooms/SciFi_Modular_Rooms_TroubleShooter_eWolf.cs
+++ eWolfSiteBuilder/_Site/Unity/SciFiModularRooms/SciFi_Modular_Rooms_TroubleShooter_eWolf.cs
@@ -34,6 +34,9 @@ namespace eWolfSiteBuilder._Site.Unity.SciFiModularRooms
             List<HTMLIndexedItems> items = new List<HTMLIndexedItems>();
 
             items.Add(new HTMLIndexedItems("Download", AddSectionDownload));
+            items.Add(new HTMLIndexedItems("Removing White Lines", AddRemovingWhiteLines));
+            items.Add(new HTMLIndexedItems("Light Flares not facing player", AddSectionFixLightFlares));
+            items.Add(new HTMLIndexedItems("Trying to read Input ", AddSectionTryingToReadInput));
             items.Add(new HTMLIndexedItems("Magenta Models", AddSectionMagenta));
 
             items.Add(new HTMLIndexedItems("Support", PageDetailsHelper.AddSectionSupport));
@@ -79,9 +82,93 @@ namespace eWolfSiteBuilder._Site.Unity.SciFiModularRooms
             outer.NewLine();
 
             outer.NewLine();
-            outer.Text(@"That you can get from Unity3D asset store");
+            outer.NewLine();
+            outer.Text("If you inport Sci-Fi Modular Rooms in to a project and find the material are megenta.");
             outer.NewLine();
             outer.NewLine();
+            outer.ImageCenter("MagentaMaterials.png", 45);
+            outer.NewLine();
+            outer.NewLine();
+            outer.EndTextCenter();
+
+            return outer.Output();
+        }
+
+        private static string AddSectionTryingToReadInput(string data)
+        {
+            HTMLBuilder outer = new HTMLBuilder();
+            outer.StartTextCenter();
+            outer.NewLine();
+
+            outer.NewLine();
+            outer.Text(@"If you get the messagse");
+            outer.NewLine();
+            outer.NewLine();
+            outer.Bold(@"You are trying to read Input using the UnityEngine.Input class, but you have switched active Input handling to Input System package in Player Settings.");
+            outer.NewLine();
+            outer.NewLine();
+            outer.Text("Then goto Project Setting -> Player");
+            outer.NewLine();
+            outer.NewLine();
+            outer.ImageCenter("PlayerSettings.png", 45);
+            outer.TextBoldText("Update ", "Active Input Handing", " to use Both");
+            outer.NewLine();
+
+            outer.EndTextCenter();
+
+            /*InvalidOperationException: You are trying to read Input using the UnityEngine.Input class, but you have switched active Input handling to Input System package in Player Settings.
+UnityEngine.Input.GetKeyDown (System.String name) (at <f54f1818644d43d89bbc43937d848865>:0)
+eWolf.Common.Helper.SceneHelpers.UpdateSnapshot () (at Assets/eWolf/Common/Scripts/Helper/SceneHelpers.cs:10)
+eWolf.SciFi_Rooms_System.Players.RoomInteractor.Update () (at Assets/eWolf/SciFi_Rooms_System/Scripts/Players/RoomInteractor.cs:45)
+
+*/
+
+            return outer.Output();
+        }
+
+        private string AddRemovingWhiteLines(string data)
+        {
+            HTMLBuilder outer = new HTMLBuilder();
+            outer.StartTextCenter();
+            outer.NewLine();
+
+            outer.NewLine();
+            outer.Text(@"If you see bits of white lines in the scene");
+            outer.NewLine();
+            outer.NewLine();
+            outer.ImageCenter("WhiteLines.PNG", 65);
+            outer.NewLine();
+            outer.NewLine();
+            outer.Text(@"Then we just need to set the camera colour to black.");
+            outer.NewLine();
+            outer.NewLine();
+            outer.ImageCenter("SetCameraColor.PNG", 30);
+
+            outer.EndTextCenter();
+
+            return outer.Output();
+        }
+
+        private string AddSectionFixLightFlares(string data)
+        {
+            HTMLBuilder outer = new HTMLBuilder();
+            outer.StartTextCenter();
+            outer.NewLine();
+
+            outer.NewLine();
+            outer.Text(@"When the light flare don't rotate to face the player.");
+            outer.NewLine();
+            outer.NewLine();
+
+            outer.ImageCenter("BrokenFlares.PNG", 54);
+            outer.NewLine();
+            outer.NewLine();
+
+            outer.Text("With in the code just update FacePlayer, to be the name of the player object you need the flares to face.");
+            outer.NewLine();
+            outer.NewLine();
+            outer.ImageCenter("Code-UpdatePlayerName.PNG", 54);
+
             outer.EndTextCenter();
 
             return outer.Output();
