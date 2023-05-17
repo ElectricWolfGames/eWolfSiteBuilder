e4bdd88addc5481aac72a9c7baf002629a7d9970
Karl Gillott <karlgillott@googlemail.com>
2023-05-17 19:32:31 +0100

    Update to use google Analytics G4

--- eWolfSiteBuilder/SiteDetails/SiteHeader.cs
+++ eWolfSiteBuilder/SiteDetails/SiteHeader.cs
@@ -13,6 +13,7 @@ namespace eWolfSiteBuilder.SiteDetails
             var offSet = pageDetails.GetRooloffSet();
             _stringBuilder.Append("<!DOCTYPE html><html lang='en' >");
             _stringBuilder.Append("	<head>");
+            AddSiteTracker(_stringBuilder);
             _stringBuilder.Append("		<meta charset='UTF-8'>");
             _stringBuilder.Append($"		<title>{pageDetails.DisplayTitle}</title>");
             _stringBuilder.Append("		<meta http-equiv='Content -Type' content='text/html; charset=UTF-8'>");
@@ -26,14 +27,35 @@ namespace eWolfSiteBuilder.SiteDetails
             _stringBuilder.Append("		<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>");
             _stringBuilder.Append("		<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>");
             //Dont't need the charts _stringBuilder.Append($"		<script type='text/javascript' src='{offSet}Scripts/chart.js'></script>");
-            _stringBuilder.Append("		<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>");
+            /*_stringBuilder.Append("		<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>");
             _stringBuilder.Append("<!-- Global site tag (gtag.js) - Google Analytics -->");
             _stringBuilder.Append("<script async src='https://www.googletagmanager.com/gtag/js?id=UA-180928318-1'></script>");
             _stringBuilder.Append("<script>");
             _stringBuilder.Append("  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);} gtag('js', new Date());  gtag('config', 'UA-180928318-1');");
-            _stringBuilder.Append("</script></head>             ");
+            _stringBuilder.Append("</script>");*/
+            _stringBuilder.Append("</head>");
 
             return _stringBuilder.ToString();
         }
+
+        private static void AddSiteTracker(StringBuilder sb)
+        {
+            /*sb.Append(@"
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=UA-180928318-1'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);} gtag('js', new Date());  gtag('config', 'UA-180928318-1');
+</script>");*/
+            sb.Append(@"<!-- Google tag (gtag.js) -->
+<script async src=""https://www.googletagmanager.com/gtag/js?id=G-XCYJV6WSHD""></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XCYJV6WSHD');
+</script>");
+        }
+
     }
 }
