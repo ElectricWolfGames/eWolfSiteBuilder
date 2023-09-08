e70f75e13376e1dc202b12f4e8d73f4359ab277e
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2023-09-08 19:25:04 +0100

    Added Version 2.1

--- eWolfSiteBuilder/_Site/Unity/DynamicVDUs/DynamicVDUs.cs
+++ eWolfSiteBuilder/_Site/Unity/DynamicVDUs/DynamicVDUs.cs
@@ -49,8 +49,9 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
             items.Add(new HTMLIndexedItems("Set-up", CreateSetUp));
             items.Add(new HTMLIndexedItems("Update Effects In Game", CreateUsingInGame));
 
-            items.Add(new HTMLIndexedItems("Using Sprites", CreateSprite));
+            //items.Add(new HTMLIndexedItems("Using Sprites", CreateSprite));
             items.Add(new HTMLIndexedItems("More Examples", CreateMoreExample));
+            items.Add(new HTMLIndexedItems("Version 1.2", CreateVerson1p2));
             items.Add(new HTMLIndexedItems("Support", PageDetailsHelper.AddSectionSupport));
 
             AddIndexItemsWithSideBar(items);
@@ -116,6 +117,16 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
 
             WebPage.CloseAllsDiv();
         }
+        private string CreateVerson1p2(string arg)
+        {
+            HTMLBuilder options = new HTMLBuilder();
+
+            options.Text("This version is now using PNG and not Jpgs");
+            options.NewLine();
+            options.Text("And updated to use Unity 2021.3.16f1");
+
+            return options.Output();
+        }
 
         private string CreateMoreExample(string arg)
         {
@@ -139,7 +150,7 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
             options.StartTextCenter();
             options.NewLine();
 
-            options.Text(@"Any number of effects can be placed on to one texture.");
+            options.Text(@"Any number of effects can be placed on one texture.");
             options.NewLine();
             options.NewLine();
             options.Image("CombinedEffects.png", 45);
@@ -176,10 +187,10 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
             options.Text("Define the size: Use the Rect setting to define the size as a percentage of the image. a width of 1 is the full width of the texture, 0.5 is half the width. And you can tick Apply Border if you want to have a border around the effect.");
             options.NewLine();
             options.NewLine();
-            options.Text("Extra Effects: If you wish to have more then one effect per texture, Increase the Size and repeat the previous two steps. However many time you like.");
+            options.Text("Extra Effects: If you wish to have more than one effect per texture, Increase the Size and repeat the previous two steps. However many times you like.");
             options.NewLine();
             options.NewLine();
-            options.Text("Colour and speed: You can update the colours used for the textures. You can change the speed of the effect by updating Interval");
+            options.Text("Colour and speed: You can update the colours used for the textures. You can change the speed of the effect by updating the Interval");
             options.NewLine();
             options.NewLine();
             options.EndTextCenter();
@@ -196,7 +207,7 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
             outer.Text(@"With the latest version, Dynamic VDU now allows you to use this effect on a sprite");
             outer.NewLine();
             outer.NewLine();
-            outer.TextBoldText(@"Demo scene has been added as ", " 'ShowSprites'", "");
+            outer.TextBoldText(@"The Demo scene has been added as ", " 'ShowSprites'", "");
             outer.NewLine();
             outer.NewLine();
             outer.EndTextCenterLeft();
@@ -229,15 +240,14 @@ namespace eWolfSiteBuilder._Site.Unity.DynamicVDUs
 
             options.Text(@"You can change the Alert value at run time by using the"); options.NewLine();
             options.Text(@"go.SendMessage(MonitorDefines.UpdateAlertMessage, newAlert);"); options.NewLine();
-            options.Text(@"Where go is the monitor object.");
+            
 
             options.NewLine();
             options.NewLine();
-            options.Text("You can pause an un-pause the effect with"); options.NewLine();
+            options.Text("You can pause and un-pause the effect with"); options.NewLine();
             options.Text("go.SendMessage(MonitorDefines.SetPauseMessage, true);"); options.NewLine();
             options.Text("And"); options.NewLine();
             options.Text("go.SendMessage(MonitorDefines.SetPauseMessage, false);"); options.NewLine();
-            options.Text("Where go is the monitor object.");
             options.NewLine();
             options.NewLine();
 
