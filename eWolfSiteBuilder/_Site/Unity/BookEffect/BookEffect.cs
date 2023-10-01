65e4587f7f0cc2163c62e559bb0ee0607d16b9c3
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2023-10-01 07:34:19 +0100

    Updted Book Effect.

--- eWolfSiteBuilder/_Site/Unity/BookEffect/BookEffect.cs
+++ eWolfSiteBuilder/_Site/Unity/BookEffect/BookEffect.cs
@@ -39,18 +39,22 @@ namespace eWolfSiteBuilder._Site.Unity.BookEffect
             WebPage.Append("<div class='row'>");
             WebPage.Append($"<div class='col-md-12'>");
 
-            List<HTMLIndexedItems> items = new List<HTMLIndexedItems>();
-
-            items.Add(new HTMLIndexedItems("Example", CreateExample));
-            items.Add(new HTMLIndexedItems("Download", CreateDownload));
-            items.Add(new HTMLIndexedItems("Inspector Options", CreateInspectorOptions));
-            items.Add(new HTMLIndexedItems("How to use", CreateHowToUse));
-            items.Add(new HTMLIndexedItems("Interface options", CreateInterfaceOptions));
-            items.Add(new HTMLIndexedItems("Project Layout", CreateProjectLayout));
-            items.Add(new HTMLIndexedItems("Support", PageDetailsHelper.AddSectionSupport));
+            var items = new List<HTMLIndexedItems>
+            {
+                new HTMLIndexedItems("Demo Video", CreateExample),
+                new HTMLIndexedItems("How to use Unity", CreateHowToUseNew),
+                new HTMLIndexedItems("How to use from Code", CreateUseFromCode),
+                new HTMLIndexedItems("Code Control Options", CreateCodeOptions),
+                
+                /*new HTMLIndexedItems("Download", CreateDownload),
+                new HTMLIndexedItems("Inspector Options", CreateInspectorOptions),
+                new HTMLIndexedItems("How to use", CreateHowToUse),
+                new HTMLIndexedItems("Interface options", CreateInterfaceOptions),
+                new HTMLIndexedItems("Project Layout", CreateProjectLayout),*/
+                new HTMLIndexedItems("Support", PageDetailsHelper.AddSectionSupport)
+            };
 
             AddIndexItemsWithSideBar(items);
-            //CreateIndexedItems(items);
 
             WebPage.EndBody();
             WebPage.Output();
@@ -97,6 +101,180 @@ namespace eWolfSiteBuilder._Site.Unity.BookEffect
         {
             HTMLBuilder options = new HTMLBuilder();
             options.Jumbotron(DisplayTitle, "Book effect lets you add a book to your scene with turning pages. And by using the interface you have actions to open, close the book and turn the pages forward and back. All pages are added to the Inspector as textures.");
+            return options.Output();
+        }
+
+        private string CreateCodeOptions(string arg)
+        {
+            HTMLBuilder options = new HTMLBuilder();
+            options.StartTextCenter();
+
+            options.Text("With access to the IBookControl");
+            options.NewLine();
+            options.NewLine();
+            options.Text("You have access to ");
+            options.NewLine();
+            options.NewLine();
+            options.EndTextCenter();
+
+            options.StartTextLeft();
+            options.Text("Can we turn the back backwards");
+            options.CodeText("bool CanTurnPageBackWard  { get; }");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Can we turn the back forward (normal direction)");
+            options.CodeText("bool CanTurnPageForward  { get; }");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Get the book details");
+            options.CodeText("IDetails GetDetails { get; }");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Is the book currently open");
+            options.CodeText("bool IsBookOpen { get; }");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Close the Book");
+            options.CodeText("void CloseBook();");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Open the Book");
+            options.CodeText("void OpenBook();");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Open the book at a set page");
+            options.CodeText("void OpenBookAtPage(int pageIndex);");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Set the animation speed for the book");
+            options.CodeText("void SetSpeed(float speed);");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Trun the page forward (normal)");
+            options.CodeText("void TurnPage();");
+            options.NewLine();
+            options.NewLine();
+
+            options.Text("Turn the page backwards");
+            options.CodeText("void TurnPageBack();");
+            options.EndTextLeft();
+
+            return options.Output();
+
+        }
+
+        private string CreateUseFromCode(string arg)
+        {
+            HTMLBuilder options = new HTMLBuilder();
+            options.StartTextCenter();
+            
+
+            options.Text("To access the book from code you will need to get the IBookControl from the Book (GameObject)");
+            options.NewLine();
+            options.NewLine();
+            options.Text("Example Code");
+            options.NewLine();
+            options.NewLine();
+            options.CodeText("public GameObject Book;</br>private IBookControl _bookControl;");
+            options.NewLine();
+            options.Text("And in the Start methods get the IBookControl");
+            options.NewLine();
+            options.NewLine();
+            options.CodeText("private void Start()</br>{</br>&nbsp&nbsp&nbsp&nbsp_bookControl = Book.GetComponent<IBookControl>();</br>}");
+            options.NewLine();
+            options.Text("In the Update code you can open the book with ");
+            options.NewLine();
+            options.NewLine();
+            options.CodeText("" +
+                "private void Update()</br>" +
+                "{<br>" +
+                "&nbsp&nbsp&nbsp&nbspif (Input.GetKeyDown(\"o\"))</br>" +
+                "&nbsp&nbsp&nbsp&nbsp{</br>" +
+                "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp_bookControl.OpenBook();</br>" +
+                "&nbsp&nbsp&nbsp&nbsp}</br>" +
+                "}");
+
+            options.NewLine();
+            options.Text("Back in Unity3D, create an empty object and add this script and then link the book game object to the Book in this script.");
+
+            options.NewLine();
+            options.Text("When you run that in game it will open the book when you press 'o'");
+            options.NewLine();
+            options.NewLine();
+            options.Text("Now you have access to the book you can add extra options like turning the pages with");
+            options.NewLine();
+            options.NewLine();
+            options.CodeText("_bookControl.TurnPage();");
+            options.NewLine();
+            options.Text("To make it safe we will first need to check the book can turn to the next page.");
+            options.NewLine();
+            options.Text("Here is the code to make the page turn from another keypress");
+            options.NewLine();
+            options.NewLine();
+            options.CodeText("" +
+                "if (Input.GetKeyDown(\"p\"))</br>" +
+                "{</br>" +
+                "&nbsp&nbsp&nbsp&nbspif (_bookControl.CanTurnPageForward)</br>" +
+                "&nbsp&nbsp&nbsp&nbsp{</br>" +
+                "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp_bookControl.TurnPage();</br>" +
+                "&nbsp&nbsp&nbsp&nbsp}</br>" +
+                "}");
+            options.NewLine();
+            options.NewLine();
+
+
+            options.EndTextCenter();
+            return options.Output();
+
+        }
+
+        private string CreateHowToUseNew(string arg)
+        {
+            HTMLBuilder options = new HTMLBuilder();
+            options.StartTextCenter();
+
+            options.TextBoldText("Add the ", @"eWolf\BookEffectV2\Prefabs\Book_Normal_pf.prefab", " into your scene.");
+            options.NewLine();
+            options.NewLine();
+            options.Text("Next you will need add the pages");
+            options.NewLine();
+            options.Image("InspectorStart.png", 45);
+            options.NewLine();
+            options.NewLine();
+            options.Text("Set the number of pages size for wish to use");
+            options.NewLine();
+            options.NewLine();
+            options.Text("Then add all the pages in to the array");
+            options.NewLine();
+            options.NewLine();
+            options.Image("InspectorEnd.png", 45);
+            options.NewLine();
+            options.NewLine();
+            options.Text("You can quicky test the book by adding the prefab");
+            options.NewLine();
+            options.Bold(@"eWolf\BookEffectV2\Prefabs\BookHudControl_pf");
+            options.NewLine();
+            options.NewLine();
+            options.Text("Then link this to your book.");
+            options.NewLine();
+            options.NewLine();
+            options.Image("InspectorHud.png", 45);
+            options.NewLine();
+            options.NewLine();
+            options.Text("And run the game and use the HUD options to use the book.");
+            options.NewLine();
+            options.NewLine();
+            options.Image("InGameTest.PNG", 45);
+
+            options.EndTextCenter();
             return options.Output();
         }
 
