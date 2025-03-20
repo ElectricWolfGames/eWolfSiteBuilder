9c317ebabaeff8b60c200daa5ee2ca14ba7fe2d9
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-03-20 20:42:17 +0000

    Added Show

---
+++ eWolfAudioSiteBuilder/_SiteDetails/Shows/Comedy/Nebulous.cs
@@ -0,0 +1,60 @@
+﻿using eWolfAudioSiteBuilder.Data;
+
+namespace eWolfAudioSiteBuilder._SiteDetails.Shows.Comedy
+{
+    public class Nebulous : AudioBaseDetails
+    {
+        public Nebulous()
+        {
+            ShowTypes = Data.Enums.ShowTypes.Comedy;
+
+            Title = "Nebulous";
+            Year = 2005;
+            DateAdded = "2025/01/10";
+
+            Description = "Set in the year 2099, the series depicts the Earth as very different from today. Following a number of environmental disasters, such as \"the Withering\" – which resulted in the loss of a great deal of human knowledge including the vacuum cleaner and fire (although the latter came back fairly quickly) – and a \"Cattle-clasm\" that killed off most of the livestock, the Earth has been reduced to a wasteland separated between \"Withered Zones\" and the remaining inhabitable areas. The Withering shifted the Earth into a new orbit, resulting in disruption to the seasons and a reformed calendar. Every day of the week is now 25 hours long except for Thursday (which, due to time anomalies, has not occurred in over a decade),[14] while the change in the length of the year led to the creation of new months such as Janril, Febtober, and Marchuary.";
+
+            Casts.Add("Professor Boffo Nebulous", "Mark Gatiss");
+            Casts.Add("Paula Breeze (assistant to Nebulous)", "Rosie Cavaliero");
+            Casts.Add("Rory Lawson (assistant to Nebulous)", "Graham Duff");
+            Casts.Add("Harry Hayes (researcher for KENT)", "Paul Putner");
+            Casts.Add("Ronald Rowlands (Minister with Unusual Portfolio)", "Graham Crowden");
+            Casts.Add("Doctor Klench (Nebulous' arch-rival)", "David Warner");
+            Casts.Add("Nebulous's mother, Gemini (KENT's resident supercomputer)", "Julia Dalkin");
+            Casts.Add("Nebulous's father", "Matt Wolf");
+
+            Production.Add("WRITER", "Graham Duff");
+            Production.Add("WRITER", "");
+            Production.Add("PRODUCER", "Ted Dowd");
+            Production.Add("DIRECTOR", "Nicholas Briggs");
+
+            Shows.Add("Series  1");
+            Shows.Shows[0].AddLink("xx1xD7iIJxs");
+            Shows.Shows[0].AddEpisode(1, "Holofile 117: The Night of the Vegetarians", "A series of mysterious deaths around Weston-Hyper-Mare leads the Professor to the last vegetarian colony, run by the notorious Dr Klench. Upon closer investigation, K.E.N.T. discover an insidious plan involving a super-intelligent cactus from space, brainwashing, extreme vegetarianism and cactus pricks.");
+            Shows.Shows[0].AddEpisode(2, "Holofile 154: The Loverly Invasion", "In a parody of the Doctor Who episode \"The Claws of Axos\", A trio of beautiful, naked men land in the withered Midlands of England, and only Professor Nebulous suspects that all may not be as it seems, claiming that they are in fact \"giant electric worms, who drool acid and bark\". Taking matters into his own hands, Nebulous attempts to kill the Lovely before a potentially terrible meeting with the world leaders.");
+            Shows.Shows[0].AddEpisode(3, "Holofile 722: The Dust Has Landed", "A nationwide attack of dust leads K.E.N.T. to Edgware, where they discover a speck of sentient dust so powerful, it necessitates the reinvention of the vacuum cleaner and a team-up of the resources of K.E.N.T. and L.O.U.G.H.B.O.R.O.U.G.H., where Nebulous meets his ex-fiancée, Erica Flazenby, and Rory discovers the joys of black helicopters and applies for a transfer.");
+            Shows.Shows[0].AddEpisode(4, "Holofile 333: Madness Is a Strange Colour", "After Sir Ronald Rowlands suffers a nervous breakdown in his newly redecorated office. K.E.N.T. investigates, and discovers that the Vartox Paint Company's new colour, Garrow, is sending people insane. With Sir Ronald and Rory driven mad, and Nebulous' leg having been broken, it is up to Paula and Harry to whiten the paint before the entire government is driven mad by their new wallpaper.");
+            Shows.Shows[0].AddEpisode(5, "Holofile 969: The Coincidence Machine", "An invention of Nebulous' mentor – a machine that observes and records coincidences – threatens the world with \"Coinci-clasm\" in which reality will become one massive coincidence; if not stopped, it will cause mankind to pass into the Weirderness, where they shall simply die out. To solve the case, the Professor must contact a terrible alternate universe: A world where everything is spelled different (literally; everything is spelled \"different\") and the curtains are a terrible pattern.");
+            Shows.Shows[0].AddEpisode(6, "Holofile 237: The Man Who Polished the Sun", "The effort to rid the world of the cold and drizzly season of Hamble is hijacked and the Earth is now endangered by a dwarf star. Klench escapes prison at this point and, aided by a treacherous Harry Hayes, attempts to use the energy of the star to hold the world to ransom. Meanwhile, Paula attempts to take Rory's advice to attract the Professor by ignoring him (she barely lasts twenty seconds).");
+
+            Shows.Add("Series  2");
+            Shows.Shows[1].AddLink("t3Ul6AX8mys");
+            Shows.Shows[1].AddEpisode(1, "Holofile 023: The Deptford Wives", "In a parody of The Stepford Wives and Jurassic Park, several men are found dead and the only link is the fact that all had recently married a cloned \"Deptford Wife\" from the Deptford Islands, who have been programmed to pamper with extreme prejudice ... one of whom has just been purchased by Harry. Guest stars Peter Davison.");
+            Shows.Shows[1].AddEpisode(2, "Holofile 993: The Buzzing", "Investigating mysterious deaths where the victims have sneezed themselves to death, Nebulous and Rory find themselves pitted against a race of bee/wasp hybrids known as the Bosps, capable of controlling the world's supply of pollen, while Paula falls under the control of the evil hive mind. Guest stars Steve Coogan.\r\n");
+            Shows.Shows[1].AddEpisode(3, "Holofile 316: I, Nebulous", "Dr. Klench reappears with a plan to murder all the delegates of a galactic peace conference, aided by the deadly fire-based aliens known as the Infernons, with his mind in the body of Professor Nebulous, purely to embarrass Nebulous, who is in charge of security for the conference.");
+            Shows.Shows[1].AddEpisode(4, "Holofile 767: Destiny of the Destinoyd", "On a trip to the decommissioned Moon base, Nebulous falls in love with a beautiful female ghost known as Franabelle, while Paula and Rory find that a \"Destinoyd\" has escaped, and may be responsible for the deaths of several men.");
+            Shows.Shows[1].AddEpisode(5, "Holofile 840: Tempus Fugitive", "Called in to investigate the groundbreaking work of scientist Doctor Linda Adnil, who has developed time travel, the K.E.N.T. team find themselves in three different years to find aspects of her that were fractured across time: Rory meets a hippie Lindavidual in 1969 at Woodstock; Harry travels to the year 7001, following the \"UltraWithering\", where everyone was reduced to single body parts, leaving Harry the most complete human in existence; and the Professor and Paula are sent to 2066 where they meet the Professor's childhood self. Guest stars Kate O'Mara.");
+            Shows.Shows[1].AddEpisode(6, "Holofile 644: Last of the Present Sirius", "Professor Nebulous is finds himself trapped in a time loop, on the weekend omnibus of a reality television series endlessly repeating like \"an insane merry-go-round, each elliptical revolution taking it further from reality, the children on the merry-go-round clinging to the brightly painted wooden horses for security, but the horses are starting to mutate, turning into Shetland Ponies\". Escaping from the show, the Professor finds himself on Sirius, the Dog Star (a.k.a. Poodle Sphere Six), coming to the end of his tenth season.");
+
+            Shows.Add("Series  3");
+            Shows.Shows[2].AddLink("Kg67SLHDIy0");
+            Shows.Shows[2].AddEpisode(1, "Holofile 001: Genesis of the Aftermath", "Nebulous meets up with Doctor Klench, who claims to have met the Byborg who have persuaded him to no longer being evil. The Byborg allow people to go back in time and correct their greatest mistakes. In Nebulous's case, this was the destruction of the Isle of Wight. He travels back to Janril 57, 2069, in order to correct his error, but it turns out to be another of Klench's traps. Meanwhile, there is something unusual about Sir Ronald's stock deals.");
+            Shows.Shows[2].AddEpisode(2, "Holofile 551: The Past Must Be Destroyed!", "Nebulous returns to his old university, Bridgeoxton, in order to solve the mystery of the disappearance of several lecturers. K.E.N.T. goes undercover, with Nebulous being a history lecturer. Rory, doing a course in Media Studies, discovers that the disappearances have been caused by the Infernons, working alongside a history lecturer who is trying to destroy the past, so there is less of it to teach. Guest stars Julia Davis.");
+            Shows.Shows[2].AddEpisode(3, "Holofile 013: The Girl With the Liquid Face", "Following the murder of the delegates at an eco-troubleshooting conference on Pancake Day Island, Nebulous, Paula, and Harry travel to Atlantis while Rory attempts to clean up for the new delegates. While the Professor battles the legendary Kraken, Paula is shocked to learn that she is actually the hybrid human/Atlantean daughter of Atlantis' queen, and Doctor Klench tries to destroy the eco summit using Atlantis' defence system.");
+            Shows.Shows[2].AddEpisode(4, "Holofile 959: We, Nebulous", "Returning to K.E.N.T. after a trip away, Paula and Rory are contacted by Nebulous, who informs them that he has been incarcerated in Biros Optimum Security Prison alongside the galaxy's most evil masterminds ... only to learn too late that they have actually rescued Nebulous' long lost evil twin brother, Professor \"Spiffo\" Nebulous, forcing K.E.N.T.'s Nebulous (whose first name is Boffo) to confront his evil twin at last.");
+            Shows.Shows[2].AddEpisode(5, "Holofile 011: Rebel Without a Cortex", "Nebulous and his team have to transport a dangerous criminal to Venus: a shapeshifting alien known as a Yarwood. It morphs into a female Loverley in front of Rory, Nebulous' father in front of the Professor, and as Nebulous himself in front of Paula.\r\n");
+            Shows.Shows[2].AddEpisode(6, "Holofile 703: Us and Phlegm", "When 99 percent of the nation's workforce phone in sick, Nebulous – the only K.E.N.T. member still in perfect health – comes face to face with a very unpleasant alien life form in the form of the Phlegmbions (A phlegm/mucus hybrid) ... allied with none other than his childhood doctor, Doctor Beep. Special Guest Star David Tennant.\r\n");
+
+        }
+    }
+}
