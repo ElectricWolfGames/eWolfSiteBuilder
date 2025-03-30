57664bae33ef7e79f4aa8e9bf5ba0f28388626c0
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-03-30 08:31:21 +0100

    updated how the show export reports TY header lines

--- eWolfAudioSiteBuilder/_Site/Audio/Shows/ShowTextCreator.cs
+++ eWolfAudioSiteBuilder/_Site/Audio/Shows/ShowTextCreator.cs
@@ -1,5 +1,6 @@
 ﻿using eWolfAudioSiteBuilder.Interfaces;
 using eWolfCommon.Helpers;
+using System.DirectoryServices.ActiveDirectory;
 using System.IO;
 using System.Text;
 
@@ -9,11 +10,42 @@ namespace eWolfAudioSiteBuilder._Site.Audio.Shows
     {
         public static void CreateYTFile(IAudioShow AudioShow)
         {
-            string path = $"E:\\_AudioTemp\\_Texts\\{FileHelper.GetSafeFileName(AudioShow.Title)}.txt";
+            string path = $"E:\\_AudioTemp\\_Texts\\{AudioShow.ShowTypes}\\";
+            Directory.CreateDirectory(path);
+            path = $"{path}\\{FileHelper.GetSafeFileName(AudioShow.Title)}.txt";
+
             StringBuilder sb = new StringBuilder();
             sb.AppendLine(AudioShow.Title);
 
-            sb.AppendLine();
+            if (AudioShow.Title == "A Plague Of Goodness")
+            {
+                int i = 0;
+                i++;
+            }
+
+            if (AudioShow.Shows.Shows.Count != 0)
+            {
+                if (AudioShow.Shows.Shows.Count > 1)
+                {
+                    for (int i = 0; i < AudioShow.Shows.Shows.Count; i++)
+                    {
+                        sb.AppendLine($"{AudioShow.Title} | Series {i + 1} of {AudioShow.Shows.Shows.Count} | Radio {AudioShow.ShowTypes}");
+                    }
+                }
+                else
+                {
+                    if (AudioShow.Shows.Shows[0].Episodes.Count == 0)
+                    {
+                        sb.AppendLine($"{AudioShow.Title} | Complete Story | Radio {AudioShow.ShowTypes}");
+                    }
+                    else
+                    {
+                        sb.AppendLine($"{AudioShow.Title} | Series 1 of 1 | Radio {AudioShow.ShowTypes}");
+                    }
+                }
+            }
+
+                sb.AppendLine();
             sb.AppendLine(AudioShow.Description);
             sb.AppendLine();
             Cast(sb, AudioShow);
