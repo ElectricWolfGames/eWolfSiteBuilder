877fece0dc06235627a5bca80024e011b97231c4
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-06-06 16:37:44 +0100

    updated show

---
+++ eWolfAudioShows/Shows/ScifiDrama/DeepStationEmerald.cs
@@ -0,0 +1,29 @@
+﻿using eWolfAudioShows.Data;
+
+namespace eWolfAudioShows.Shows.ScifiDrama
+{
+    public class DeepStationEmerald : AudioBaseDetails
+    {
+        public DeepStationEmerald()
+        {
+            ShowTypes = Data.Enums.ShowTypes.SciFiDrama;
+
+            Title = "Deep Station Emerald";
+            Year = 1996;
+            DateAdded = "";
+
+            Description = "Joe Turner’s drama is a futuristic undersea thriller. The crew of a research base on the ocean floor discovers a source of cold fusion, the solution to all the world’s energy problems. Elation turns to terror, however, when a series of ‘accidents’ causes several deaths, and a strange virus which mutates DNA is loosed… obviously, someone on board is a killer…";
+
+            Casts.Add("Rossi", "Maureen Beattie");
+            Casts.Add("Kettle", "Tom Georgeson");
+            Casts.Add("Captain Harris", "Danny Webb");
+            Casts.Add("O'Connor", "Ricky Tomlinson");
+
+            Production.Add("WRITER", "Joe Turner");
+            Production.Add("DIRECTOR", "Martin Jameson");
+
+            Shows.Add(string.Empty);
+            Shows.Shows[0].AddLink("");
+        }
+    }
+}
