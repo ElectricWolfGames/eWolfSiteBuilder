ea7163ee34ce3c8757bced1cdcfa246cd62766ef
Karl Gillott <39939684+ElectricWolfGames@users.noreply.github.com>
2025-07-04 06:54:43 +0100

    added new shows

---
+++ eWolfAudioShows/Shows/Comedy/CenturyVampire.cs
@@ -0,0 +1,40 @@
+﻿using eWolfAudioShows.Data;
+
+namespace eWolfAudioShows.Shows.Comedy
+{
+    public class CenturyVampire : AudioBaseDetails
+    {
+        public CenturyVampire()
+        {
+            ShowTypes = eWolfAudioShows.Data.Enums.ShowTypes.Comedy;
+
+            Title = "20th Century Vampire";
+            Year = 1993;
+            DateAdded = "";
+
+            Description = "The adventures of trainee vampire Eloise, her boyfriend Wayne and her eccentric Transylvanian tutor, Lucretia";
+
+            Casts.Add("Eloise", "Louise Lombard");
+            Casts.Add("Lucretia", "Joanna Kanska");
+
+            Casts.Add("Wayne", "William Ivory");
+            Casts.Add("Debbie", "Jane Hazlegrove");
+            Casts.Add("Mr. Jenkins", "Fine Time Fontayne");
+
+            Production.Add("WRITER", "Joe Turner");
+            Production.Add("WRITER", "");
+
+            Production.Add("PRODUCER", "");
+            Production.Add("DIRECTOR", "");
+
+            Shows.Add("Series  1");
+            Shows.Shows[0].AddLink("");
+            Shows.Shows[0].AddEpisode(1, "I Am a Vampire", "Eloise receives monumental news from her mysterious Aunt Lucretia; it seems the Co-op checkout girl is now a vampire!");
+            Shows.Shows[0].AddEpisode(2, "Back to School", "GCSE vampirism exam-time, but at least there is a practical!");
+            Shows.Shows[0].AddEpisode(3, "The Biggest Secret", "Bursting to share her news of her vampirism, Eloise tells each of her possible confidants a dark secret (all untrue) to see if they can be trusted.");
+            Shows.Shows[0].AddEpisode(4, "Staff Outing", "Debbie is responsible for arranging the annual Co-op staff outing, and decides on a trip to donate blood.");
+            Shows.Shows[0].AddEpisode(5, "Endangered Species", "Eloise and Lucretia discover that Mr Jenkins's blood is the rarest – and tastiest! – in the world.");
+            Shows.Shows[0].AddEpisode(6, "Love Shack", "Left alone to castle-sit in Lucretia's absence, how far will Eloise let boyfriend Wayne go?");
+        }
+    }
+}
---
+++ eWolfAudioShows/Shows/Comedy/PleaseUseOtherDoor.cs
@@ -0,0 +1,63 @@
+﻿using eWolfAudioShows.Data;
+
+namespace eWolfAudioShows.Shows.Comedy
+{
+    public class PleaseUseOtherDoor : AudioBaseDetails
+    {
+        public PleaseUseOtherDoor()
+        {
+            ShowTypes = eWolfAudioShows.Data.Enums.ShowTypes.Comedy;
+
+            Title = "Please Use Other Door";
+            Year = 1993;
+            DateAdded = "";
+
+            Description = "";
+
+            Casts.Add("", "Madeline Appiah");
+            Casts.Add("", "Gabby Best");
+            Casts.Add("", "Will Hartley");
+            Casts.Add("", "Ray Sesay");
+            Casts.Add("", "Rebecca Shorrocks");
+            Casts.Add("", "Toby Williams");
+            Casts.Add("", "Chris Ryman");
+            Casts.Add("", "Witney White");
+            Casts.Add("", "Jason Forbes");
+            Casts.Add("", "Rosie Holt");
+
+            Production.Add("WRITER", "Anna Goodman");
+            Production.Add("WRITER", "Jeffrey Aidoo");
+            Production.Add("WRITER", "Simon Alcock");
+            Production.Add("WRITER", "Daniel Audritt");
+            Production.Add("WRITER", "Ed Amsden");
+            Production.Add("WRITER", "Tom Coles");
+            Production.Add("WRITER", "Kat Butterfield");
+            Production.Add("WRITER", "Bill Dare");
+            Production.Add("WRITER", "Rob Darke");
+            Production.Add("WRITER", "David McIver");
+            Production.Add("WRITER", "Chazz Redhead");
+            Production.Add("WRITER", "Jack Robertson");
+            Production.Add("WRITER", "Edward Tew");
+            Production.Add("WRITER", "Sophie Dickson");
+            Production.Add("WRITER", "Laura Major");
+
+            Production.Add("PRODUCER", "Bill Dare");
+            Production.Add("SOUND DESIGNER", "Rich Evans");
+            Production.Add("DIRECTOR", "");
+
+            Shows.Add("Series  1");
+            Shows.Shows[0].AddLink("");
+            Shows.Shows[0].AddEpisode(1, "Episode One", "A satirical take on modern life. Naturalistic sketches taking a sometimes dark look at the world of work, relationships, institutions and families. Highlights include a goose enrolling as a police officer - and somehow managing to make it through the very tight vetting procedures. There's a character sceptical about time, and a doll-baby so realistic she's more of a nightmare than a toy.");
+            Shows.Shows[0].AddEpisode(2, "Episode Two", "The second show includes a series of sketches in which someone is forced to watch Succession, a man applies for a business loan for a company whose aim is doing evil and a woman dates the Pied Piper.");
+            Shows.Shows[0].AddEpisode(3, "Episode Three", "We attend the Schadenfreude Experience, there is a visit to a pet shop to buy a giraffe, and why you need a sense of humour to be a heart surgeon.");
+            Shows.Shows[0].AddEpisode(4, "Episode Four", "Some parents tell their daughter the truth about Father Christmas, there is a friendly chat during a rugby scrum, and we witness a polite mugging.");
+
+            Shows.Add("Series  2");
+            Shows.Shows[1].AddLink("");
+            Shows.Shows[1].AddEpisode(1, "Episode One", "Can you change your sexual orientation at will? Is it ever fair to blame the children for a divorce? What happens if a therapist brings their own emotional baggage to the treatment room?");
+            Shows.Shows[1].AddEpisode(2, "Episode Two", "How can a tea-cosey help with dating decisions? What would football fans chant if they could express their true feelings? What are some of the do's and don'ts of role play?");
+            Shows.Shows[1].AddEpisode(3, "Episode Three", "Can families be merged like companies? Should ransom notes be aesthetically pleasing? Why do people think truffle hunting in Tuscany would be the ideal life?");
+            Shows.Shows[1].AddEpisode(4, "Episode Four", "How would middle class liberals plan a jewellery heist? What are the categories for the new Modern Man Awards? Why are people in care homes forced into group singing?");
+        }
+    }
+}
